485c3a2ccb98a70e06abea49f6bec9f1407ac3f7
Corrado LONGONI <corradolongoni@Corrados-MacBook-Pro.local>
2020-06-26 21:59:16 +0200

    with devor camera

--- src/index.js
+++ src/index.js
@@ -108,7 +108,7 @@ var scene = new Scene(engine);
 // var camera = new FreeCamera("camera1", new Vector3(0, 3, -5), scene);
 
 // This creates and positions a device orientation camera 	
-var camera = new DeviceOrientationCamera("DevOr_camera", new BABYLON.Vector3(0, 0, 0), scene);
+var camera = new DeviceOrientationCamera("DevOr_camera", new Vector3(0, 3, -5), scene);
 
 // This targets the camera to scene origin
 camera.setTarget(new Vector3(0, 0, 50))
