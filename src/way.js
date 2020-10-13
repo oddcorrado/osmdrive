8b1442e3e5be96840b10b8f576b2f8bc7180e3df
oddarock <corradolongoni@gmail.com>
2020-10-13 19:42:23 +0200

    intersections at last

--- src/way.js
+++ src/way.js
@@ -14,6 +14,60 @@ import { scene as globalScene } from './index'
 // import { Quaternion } from '@babylonjs/core/Maths/math'
 
 
+var eps = 0.0000001;
+function between(a, b, c) {
+    return a-eps <= b && b <= c+eps;
+}
+function segmentIntersection(x1,y1,x2,y2, x3,y3,x4,y4) {
+    var x=((x1*y2-y1*x2)*(x3-x4)-(x1-x2)*(x3*y4-y3*x4)) /
+            ((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4))
+    var y=((x1*y2-y1*x2)*(y3-y4)-(y1-y2)*(x3*y4-y3*x4)) /
+            ((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4))
+    if (isNaN(x)||isNaN(y)) {
+        return false
+    } else {
+        if (x1>=x2) {
+            if (!between(x2, x, x1)) {return false}
+        } else {
+            if (!between(x1, x, x2)) {return false}
+        }
+        if (y1>=y2) {
+            if (!between(y2, y, y1)) {return false}
+        } else {
+            if (!between(y1, y, y2)) {return false}
+        }
+        if (x3>=x4) {
+            if (!between(x4, x, x3)) {return false}
+        } else {
+            if (!between(x3, x, x4)) {return false}
+        }
+        if (y3>=y4) {
+            if (!between(y4, y, y3)) {return false}
+        } else {
+            if (!between(y3, y, y4)) {return false}
+        }
+    }
+    return {x: x, y: y}
+}
+
+function vectorIntersection(v1, v2, u1, u2) {
+    const x1 = v1.x
+    const y1 = v1.z
+    const x2 = v2.x
+    const y2 = v2.z
+    const x3 = u1.x
+    const y3 = u1.z
+    const x4 = u2.x
+    const y4 = u2.z
+
+    const inter = segmentIntersection(x1, y1, x2, y2, x3, y3, x4, y4)
+    if(!inter) { return null }
+
+    return new Vector3(inter.x, v1.y, inter.y) // TODO user ratio for ys
+}
+
+// console.log("INTER", segment_intersection (-10, -10, 20, 20, -10, 10, 10, -10 ))
+
 const paths = []
 
 let enableDebug = false
@@ -37,10 +91,13 @@ export default function createWays(scene, planes) {
     rootPaths = createRootPaths(ways)
 
     // create the lanes
-    rootLanes = createRootLanes(rootPaths)
+    roads = createRoads(rootPaths)
 
     // update the root lanes junctions
-    // projectJunctions(rootLanes)
+    // projectJunctions(roads)
+    roads = extendroads(roads)
+    updateNodeIndexes()
+    createAllIntersectionNodes() 
 
     // remove old junctions from root lanes
 
@@ -72,10 +129,16 @@ export default function createWays(scene, planes) {
     })
 }
 
+
+// ***** LOGICAL WAYS
+// ***** TO MOVE IN A SEPARATE FILE
+
 const rootJunctions = []
 let rootPaths = []
-let rootLanes = []
+let roads = []
 let rootLaneJunctions = []
+let intersectionNodes = []
+let rootLaneProjections = []
 
 // Creates an array of root junctions based on point distance
 function createRootJunctions(ways) {
@@ -110,31 +173,44 @@ function createRootPaths(ways) {
     return paths
 }
 
+function rootLaneJunctionInsert(node) {
+    let junction = rootLaneJunctions.find(j => j.junctionIndex === node.junctionIndex)
+    if( junction == null) {
+        const junction = {
+            junctionIndex: node.junctionIndex,
+            nodes: [node]
+        }
+        rootLaneJunctions.push(junction)
+    } else {
+        junction.nodes.push(node)
+    }
+ }
+
 // create the root lanes based on the paths
-function createRootLanes(paths) {
+function createRoads(paths) {
     const up = new Vector3(0, 1, 0)
     const down = new Vector3(0, -1, 0)
     let nodeId = 0;
 
-    const lanes = paths.map((path, pathIndex) => {
+    const lanes = paths.map((path, roadIndex) => {
         const left = path.map((node, nodeIndex) => {
             const index = Math.max(nodeIndex, 1)
             const current = new Vector3(path[index].point.x, path[index].point.y, path[index].point.z)
             const prev = new Vector3(path[index - 1].point.x, path[index - 1].point.y, path[index - 1].point.z)
             const point = node.point.add(current.subtract(prev).cross(up).normalize().scale(2))
-            if(node.junctionIndex !== -1) {
-                rootLaneJunctions.push({
-                    laneIndex: pathIndex,
-                    nodeId,
-                    isLeft: true,
-                    junctionIndex: node.junctionIndex
-                })
-            }
-            return ({
+            const newNode = {
+                roadIndex,
+                laneIndex: 0,
                 point,
                 nodeId,
+                nodeIndex,
+                upwise: true,
                 junctionIndex: node.junctionIndex
-            })
+            }
+            if(node.junctionIndex !== -1) {
+                rootLaneJunctionInsert(newNode)
+            }
+            return newNode
         })
         nodeId++
         const right = path.map((node, nodeIndex) => {
@@ -142,81 +218,155 @@ function createRootLanes(paths) {
             const current = new Vector3(path[index].point.x, path[index].point.y, path[index].point.z)
             const prev = new Vector3(path[index - 1].point.x, path[index - 1].point.y, path[index - 1].point.z)
             const point = node.point.add(current.subtract(prev).cross(down).normalize().scale(2))
-            if(node.junctionIndex !== -1) {
-                rootLaneJunctions.push({
-                    laneIndex: pathIndex,
-                    nodeId,
-                    isLeft: false,
-                    junctionIndex: node.junctionIndex
-                })
-            }
-            return ({
+            const newNode = {
+                roadIndex,
+                laneIndex: 1,
                 point,
                 nodeId,
+                nodeIndex,
+                upwise: false,
                 junctionIndex: node.junctionIndex
-            })
+            }
+            if(node.junctionIndex !== -1) {
+                rootLaneJunctionInsert(newNode)
+            }
+            return newNode
         })
-        return ({left, right})
+        return ({lanes: [left, right]})
     })
     return lanes
 }
 
-function getNodeIndexInLane(laneIndex, isLeft, nodeId) {
-    const path = rootLanes[laneIndex]
-    const lane = isLeft ? path.left : path.right
-    let node = null
+function extendSegment(n1, n2, scale) {
+    const v = new Vector3(n2.point.x - n1.point.x, n2.point.y - n1.point.y, n2.point.z - n1.point.z)
+    const newPoint = new Vector3(n2.point.x, n2.point.y, n2.point.z).add(v.normalize().scale(scale))
 
-    return lane.findIndex(n => n.nodeId === nodeId)
+    return ({
+        point: newPoint,
+        nodeId: 556,
+        junctionIndex: -1
+    })
 }
 
-function getNodeInLane(laneIndex, isLeft, index) {
-    const path = rootLanes[laneIndex]
-    const lane = isLeft ? path.left : path.right
-    let node = null
+function extendLane(lane) {
+    let node0 = extendSegment(lane[1], lane[0], 4)
+    let nodeN = extendSegment(lane[lane.length - 2], lane[lane.length - 1], 4)
 
-    return lane[index]
+    lane.push(nodeN)
+    lane.unshift(node0)
+
+    return lane
 }
 
-// DEBUG ME
-function projectJunctions(lanes) {
-    console.log("PROJECT", rootLaneJunctions)
-    lanes.forEach((lane, laneIndex) => {
-        lane.left.forEach((node) => {
-            if(node.junctionIndex != -1) {
-                const siblings = rootLaneJunctions.filter( j => 
-                    j.junctionIndex === node.junctionIndex
-                    && (!j.isLeft
-                    || j.laneIndex !== laneIndex))
-                console.log("PROJECT", node.junctionIndex, laneIndex, siblings)
-                siblings.forEach(s => {
-                    const index = getNodeIndexInLane(s.laneIndex, s.isLeft, s.nodeId)
-                    if(index !== -1 && index < lanes[s.laneIndex].left.length - 1) {
-                        console.log("PROJECT", s.laneIndex, s.isLeft, index)
-                        const sNode = getNodeInLane(s.laneIndex, s.isLeft, index)
-                        const sNodeN = getNodeInLane(s.laneIndex, s.isLeft, index + 1)
+function extendroads(lanes) {
+    lanes.forEach(lane => {
+        lane.lanes[0] = extendLane(lane.lanes[0])
+        lane.lanes[1] = extendLane(lane.lanes[1])
+    })
+    return lanes
+}
 
-                        const u = new Vector3(sNodeN.point.x - sNode.point.x, sNodeN.point.y - sNode.point.y, sNodeN.point.z - sNode.point.z)
-                        const v = new Vector3(node.point.x - sNode.point.x, node.point.y - node.point.y, node.point.z - node.point.z)
+const rootIntersections = []
+function createIntersectionNodesFromNodeSegment(segment, otherSegments) {
+    otherSegments.forEach(otherSegment => {
+        const inter = vectorIntersecton(segment[0], segment[1], otherSegment[0], otherSegment[1])
 
-                        const dot = Vector3.Dot(u, v)
+        if(inter != null) {
+            rootIntersections.push(inter)
+        }
+    })
+}
 
-                        const tx = u.scale(Math.sqrt(Math.abs(dot)) / u.length())
+function getNodeInLane(roadIndex, laneIndex, index) {
+    const road = roads[roadIndex]
 
-                        const newVector = tx.add(new Vector3(sNode.point.x, sNode.point.y, sNode.point.w))
-                        const newNode = {
-                            point : { x: newVector.x, y: newVector.y, z: newVector.z },
-                            nodeId : 33,
-                            junctionIndex: node.junctionIndex
-                        }
+    return road.lanes[laneIndex]
+}
 
-                        console.log("PROJECT", sNode, newNode, sNodeN)
-                    }
-                    
-                })
-            }
+function updateNodeIndexes() {
+    roads.forEach((road, roadIndex) => {
+        road.lanes.forEach((lane, laneIndex) => {
+            lane.forEach((node, nodeIndex) => {
+                node.nodeIndex = nodeIndex
+                node.laneIndex = laneIndex
+                node.roadIndex = roadIndex
+            })
         })
     })
 }
+
+function getNodeInRoad(roadIndex, laneIndex, nodeIndex) {
+    if(nodeIndex < 0) { return null }
+    const lane = roads[roadIndex].lanes[laneIndex]
+    if(nodeIndex >= lane.length) { return null }
+    if(lane[nodeIndex] == null) { console.log(roadIndex, laneIndex, nodeIndex, lane) }
+    return lane[nodeIndex]
+}
+// gets previous and next nodes from a given lane node
+// make sure node index are clean before using this
+function getPrevNext(node) {
+    const prev = getNodeInRoad(node.roadIndex, node.laneIndex, node.nodeIndex - 1)
+    const next = getNodeInRoad(node.roadIndex, node.laneIndex, node.nodeIndex + 1)
+    return {prev, next}
+}
+
+function getInsertionSegment(a1, a2, b1, b2) {
+    return ({
+        roadIndex: a1.roadIndex,
+        laneIndex: a1.laneIndex,
+        connections: [a1, a2, b1, b2],
+        nodeIndexPrev: Math.min(a1.nodeIndex, a2.nodeIndex),
+        nodeIndexNext: a2.nodeIndex
+    })
+}
+
+function addIntersection(a1, a2, b1, b2) {
+    const point = vectorIntersection(a1.point, a2.point, b1.point, b2.point)
+    if(point != null) {
+        intersectionNodes.push({
+            insertionSegments: [
+                getInsertionSegment(a1, a2, b1, b2),
+                getInsertionSegment(b1, b2, a1, a2)
+            ],
+            point
+        })
+    }
+}
+
+function createIntersectionNodesFromNode(node, otherNodes) {
+    const { prev, next } = getPrevNext(node)
+
+    if(prev == null || next == null) { console.error("bad node / prev, node, next", prev, node, ext); return }
+    otherNodes.forEach(other => {
+        const { prev: oPrev, next: oNext } = getPrevNext(other)
+        if (oPrev == null || oNext == null) { console.error("bad other / prev, other, next", oPrev, other, oNext); return }
+
+        addIntersection(node, prev, other, oNext)
+        addIntersection(node, next, other, oNext)
+        addIntersection(node, prev, other, oPrev)
+        addIntersection(node, next, other, oPrev)
+    }) 
+}
+
+function createIntersectionNodesFromJunction(junction) {
+    junction.nodes.forEach(node => {
+        const otherNodes = junction.nodes.filter(n => n.nodeId !== node.nodeId)
+        createIntersectionNodesFromNode(node, otherNodes)
+    })
+}
+
+function createAllIntersectionNodes() {
+    rootLaneJunctions.forEach((junction, junctionIndex) => {
+        createIntersectionNodesFromJunction(junction)
+    })
+}
+
+function getNodeIndexInLane(roadIndex, laneIndex, nodeId) {
+    const road = roads[roadIndex]
+
+    return road.lanes[laneIndex].findIndex(n => n.nodeId === nodeId)
+}
+
 
 //
 function getInterPos(curr, next){
@@ -301,34 +451,46 @@ export function toggleDebugWays() {
     enableDebug = !enableDebug
 
     rootPaths.forEach((path, i) => {
-        console.log(path._curve)
         const curve = path.map(n => n.point)
         let li = Mesh.CreateLines('li', curve, globalScene)
         li.position.y = li.position.y + 0.1
         li.color = Color3.Red()
         path.forEach(node => {
             if(node.junctionIndex > 0) {
-                let m = MeshBuilder.CreateSphere("sphere", {radius: 1}, globalScene)
-                m.position = node.point
+               // let m = MeshBuilder.CreateSphere("sphere", {diameter: 0.4}, globalScene)
+               //  m.position = node.point
             }
 
         })
     })
 
-    rootLanes.forEach((lane, i) => {
-        const curveLeft = lane.left.map(n => n.point)
-        const curveRight = lane.right.map(n => n.point)
-        let liLeft = Mesh.CreateLines('li', curveLeft, globalScene)
-        let liRight = Mesh.CreateLines('li', curveRight, globalScene)
-        liLeft.position.y = liLeft.position.y + 0.1
-        liRight.position.y = liRight.position.y + 0.1
-        liLeft.color = Color3.Blue()
-        liRight.color = Color3.Green()
-        /* path.forEach(node => {
-            if(node.junctionIndex > 0) {
-                let m = MeshBuilder.CreateSphere("sphere", {radius: 1}, globalScene)
-                m.position = node.point
-            }
-        }) */
+    roads.forEach((road, i) => {
+        road.lanes.forEach((lane) => {
+            const curve = lane.map(n => n.point)
+            let li = Mesh.CreateLines('li', curve, globalScene)
+            li.position.y = li.position.y + 0.1
+            li.position.y = li.position.y + 0.1
+            li.color = Color3.Random()
+            lane.forEach(node => {
+                if(node.junctionIndex > 0) {
+                    let m = MeshBuilder.CreateBox("box", {size: 0.6}, globalScene)
+                    m.position = node.point
+                } else {
+                    let m = MeshBuilder.CreateBox("box", {size: 0.2}, globalScene)
+                    m.position = node.point
+                }
+            })
+        })
+    })
+
+    intersectionNodes.forEach(intersection => {
+        console.log(intersection)
+        let m = MeshBuilder.CreateSphere("sphere", {diameter: 0.5}, globalScene)
+        m.position = intersection.point
+    })
+
+    rootLaneProjections.forEach(node => {
+       // let m = MeshBuilder.CreateSphere("sphere", {radius: 0.7}, globalScene)
+       // m.position = node.point
     })
 }
